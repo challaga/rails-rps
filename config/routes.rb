456e239f479573f77c1ca2af022bc93cf55a6b8c
challaga <146873313+challaga@users.noreply.github.com>
2023-10-25 14:30:43 +0000

    halfway class

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "game", :action => "user_played_rock"})
+
+  get("/paper", { :controller => "game", :action => "user_played_paper"})
+
+  get("/dynamic/:the_move", { :controller => "game", :action => "flexible"})
 
 end
